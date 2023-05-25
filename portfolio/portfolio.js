292a7cc28d3d9d326b9dc5e7f8c3b8d861c5ec80
DebSofia <deb.sofiasilva@gmail.com>
2023-05-25 19:05:41 +0100

    Portfolio page
    Finished the portfolio page.
    Styled the page with Css and bootstrap.
    Added a slider with html and Css.
    Made small changes to the buttons.

---
+++ portfolio/portfolio.js
@@ -0,0 +1,22 @@
+/*const images = [
+  "./../media/1.jpg",
+  "./../media/2.jpg",
+  "./../media/3.jpg",
+  "./../media/4.jpg",
+  "./../media/5.jpg",
+  "./../media/6.jpg",
+  "./../media/7.jpg",
+];
+
+counter = 0;
+
+setInterval(() => {
+  const image = images[counter];
+  document.querySelector(
+    "#gallery"
+  ).innerHTML = `<div><img class="gallery-image" src="${image}"/></div>`;
+  counter += 1;
+  if (counter == images.length) {
+    counter = 0;
+  }
+}, 5000);*/
